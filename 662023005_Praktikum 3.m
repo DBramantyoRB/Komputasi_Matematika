1f6be77f571ae0869bc0612bf7ea25495ee21881
unknown <ranudaniel88@gmail.com>
2025-04-14 14:01:58 +0700

    praktikum 2

---
+++ 662023005_Praktikum 3.m
@@ -0,0 +1,15 @@
+p = input('Masukan Nilai')
+if p > 70
+    disp('anda lulus');
+end
+disp('coba lagi');
+n = input('Masukan ')
+if n >= 190
+disp('Sangat Tinggi');
+elseif n > 160 && n < 190
+    disp('cukup tinggi')
+elseif n > 0 && n < 159
+    disp('Tinggi')
+elseif n < 0
+disp('Not Human');
+end
